77b1f83e67ab65de66e5e93fe6a6a6287579a663
ChinnyChilla <michaelzheng04@gmail.com>
2025-12-20 00:16:48 -0500

    inital

---
+++ Stacks/Stacks/StacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StacksApp.swift
+//  Stacks
+//
+//  Created by Michael Zheng on 12/20/25.
+//
+
+import SwiftUI
+
+@main
+struct StacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
